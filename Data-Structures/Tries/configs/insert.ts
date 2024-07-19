18ed33c7081b78d0ca6e57f4a2c14ef72994e79c
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-07-19 23:18:21 +0530

    trie

---
+++ Data-Structures/Tries/configs/insert.ts
@@ -0,0 +1,10 @@
+insert(word: string): void {
+    let node: TrieNode = this.root;
+    for (let char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    }
+    node.isEndOfWord = true;
+  }
