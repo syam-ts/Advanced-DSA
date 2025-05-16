676bf8ca8fde65ff433b2f39481f7476436ce517
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-05-16 18:37:07 +0530

    fix: add description for insertion  sort

--- Algorithms/Sorting/insertionSort.ts
+++ Algorithms/Sorting/insertionSort.ts
@@ -1,7 +1,7 @@
 var arr: number[] = [5, 3, 1, 2, 23, 32, 1, 35, 8];
 
 const insertionSort = (arr: number[]): number[] => {
-    
+
     for (let i = 0; i < arr.length; i++) {
         let current: number = arr[i];
         let compareIdx: number = i - 1;
@@ -15,3 +15,9 @@ const insertionSort = (arr: number[]): number[] => {
 };
 
 console.log("Result: ", insertionSort(arr));
+
+
+/* pick the left side element, 
+insert the big element to the right side
+ until fit the proper position
+*/
