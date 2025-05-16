986212240eae7a8e5ccbc001f2130e2a3b5919ac
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-05-16 18:31:03 +0530

    chore: add insertion sort

---
+++ Algorithms/Sorting/insertionSort.ts
@@ -0,0 +1,17 @@
+var arr: number[] = [5, 3, 1, 2, 23, 32, 1, 35, 8];
+
+const insertionSort = (arr: number[]): number[] => {
+    
+    for (let i = 0; i < arr.length; i++) {
+        let current: number = arr[i];
+        let compareIdx: number = i - 1;
+        while (compareIdx >= 0 && arr[compareIdx] > current) {
+            arr[compareIdx + 1] = arr[compareIdx];
+            compareIdx--;
+        }
+        arr[compareIdx + 1] = current;
+    }
+    return arr;
+};
+
+console.log("Result: ", insertionSort(arr));
